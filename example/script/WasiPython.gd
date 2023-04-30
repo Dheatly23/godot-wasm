232ddeb3f56fe1698c8ba41464fb5db13f3852be
Dheatly23 <71598333+Dheatly23@users.noreply.github.com>
2023-04-30 21:50:28 +0700

    Minor Update to Non-Threadpooled WasiPython

--- example/script/WasiPython.gd
+++ example/script/WasiPython.gd
@@ -36,33 +36,6 @@ func __log(msg: String) -> void:
 func __bin_log(msg: PoolByteArray) -> void:
 	emit_signal("message_emitted", msg.get_string_from_utf8())
 
-# Non threadpool version
-#func _ready():
-#	var f: WasmFile = load(wasm_file)
-#
-#	var module := f.get_module()
-#	if module == null:
-#		__log("Cannot compile module " + wasm_file)
-#		return
-#
-#	wasi_ctx = WasiContext.new()
-#	wasi_ctx.connect("stdout_emit", self, "__bin_log")
-#	wasi_ctx.connect("stderr_emit", self, "__bin_log")
-#	wasi_ctx.bypass_stdio = false
-#	wasi_ctx.write_memory_file("/test.py", PROGRAM.to_utf8())
-#
-#	var instance = WasmInstance.new().initialize(
-#		module,
-#		{},
-#		{
-#			"engine.use_epoch": true,
-#			"engine.use_wasi": true,
-#			"wasi.wasi_context": wasi_ctx,
-#			"wasi.args": ["rustpython.wasm", "test.py"],
-#		}
-#	)
-#	instance.call_wasm("__main_void", [])
-
 func __run():
 	var module := file.get_module()
 	if module == null:
@@ -81,3 +54,22 @@ func __run():
 		}
 	)
 	instance.call_queue("_start", [])
+
+# Non threadpool version
+#func __run():
+#	var module := file.get_module()
+#	if module == null:
+#		__log("Cannot compile module " + wasm_file)
+#		return
+#
+#	var instance = WasmInstance.new().initialize(
+#		module,
+#		{},
+#		{
+#			"engine.use_epoch": true,
+#			"engine.use_wasi": true,
+#			"wasi.wasi_context": wasi_ctx,
+#			"wasi.args": ["rustpython.wasm", "test.py"],
+#		}
+#	)
+#	instance.call_wasm("__main_void", [])
