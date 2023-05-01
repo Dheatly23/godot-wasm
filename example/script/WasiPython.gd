a4c9bf24a47b581135d276f0e724b2d05678a7fd
Dheatly23 <71598333+Dheatly23@users.noreply.github.com>
2023-05-01 20:59:36 +0700

    Updates RustPython Example

--- example/script/WasiPython.gd
+++ example/script/WasiPython.gd
@@ -2,18 +2,9 @@ extends Control
 
 signal message_emitted(msg)
 
-const PROGRAM := """
-print('Hello from RustPython!')
-
-print('First 10 fibonacci numbers:')
-
-a, b = 1, 1
-for i in range(10):
-	print(a)
-	a, b = b, a + b
-"""
-
 export(String, FILE, "*.wasm,*.wat") var wasm_file := ""
+
+onready var code_textbox: TextEdit = $Container/Panel/Margin/Tab/VBox/Code
 
 onready var file: WasmFile = load(wasm_file)
 onready var wasi_ctx: WasiContext = WasiContext.new()
@@ -28,15 +19,51 @@ func _ready():
 	wasi_ctx.connect("stdout_emit", self, "__bin_log")
 	wasi_ctx.connect("stderr_emit", self, "__bin_log")
 	wasi_ctx.bypass_stdio = false
-	wasi_ctx.write_memory_file("/test.py", PROGRAM.to_utf8())
+	wasi_ctx.write_memory_file("hello_world.py", "print('Hello from Python!')")
+	wasi_ctx.write_memory_file("primes.py", """
+def primes(n):
+	r = [2]
+	for i in range(3, n):
+		if all(i % j for j in r):
+			r.append(i)
+	return r
+
+print("First 1000 primes:")
+p = primes(1000)
+for i in range(0, len(p), 16):
+	print(*(f"{x:3d}" for x in p[i:i+16]))
+""")
+	wasi_ctx.write_memory_file("data/text.txt", """
+A text data read by read_file.py
+
+I don't really feel like putting Lorem Ipsum here :)
+""")
+	wasi_ctx.write_memory_file("read_file.py", """
+print("Reading data/text.txt")
+with open("data/text.txt", "rt") as f:
+	for l in f:
+		print(l.strip())
+""")
+	wasi_ctx.write_memory_file("write_file.py", """
+import json
+
+print("Writing data/output.json")
+with open("data/output.json", "wt") as f:
+	json.dump({
+		"first_name": "Jack",
+		"last_name": "Coe",
+		"alive": False,
+		"profession": "preacher",
+	}, f)
+""")
 
 func __log(msg: String) -> void:
 	emit_signal("message_emitted", msg)
 
 func __bin_log(msg: PoolByteArray) -> void:
-	emit_signal("message_emitted", msg.get_string_from_utf8())
+	emit_signal("message_emitted", msg.get_string_from_utf8().strip_edges())
 
-func __run():
+func __run(source: String, ret_method = ""):
 	var module := file.get_module()
 	if module == null:
 		__log("Cannot compile module " + wasm_file)
@@ -50,13 +77,18 @@ func __run():
 			"engine.use_epoch": true,
 			"engine.use_wasi": true,
 			"wasi.wasi_context": wasi_ctx,
-			"wasi.args": ["rustpython.wasm", "test.py"],
+			"wasi.args": ["rustpython.wasm", source],
 		}
 	)
-	instance.call_queue("_start", [])
+	instance.call_queue(
+		"_start",
+		[],
+		self if ret_method != "" else null,
+		ret_method
+	)
 
 # Non threadpool version
-#func __run():
+#func __run(source: String):
 #	var module := file.get_module()
 #	if module == null:
 #		__log("Cannot compile module " + wasm_file)
@@ -69,7 +101,27 @@ func __run():
 #			"engine.use_epoch": true,
 #			"engine.use_wasi": true,
 #			"wasi.wasi_context": wasi_ctx,
-#			"wasi.args": ["rustpython.wasm", "test.py"],
+#			"wasi.args": ["rustpython.wasm", source],
 #		}
 #	)
-#	instance.call_wasm("__main_void", [])
+#	var r = instance.call_wasm("_start", [])
+#	if ret_method != "":
+#		self.call(ret_method, r)
+
+func __cb_write_file(_v):
+	var b = wasi_ctx.read_memory_file("data/output.json", 1000)
+	if not (b is PoolByteArray):
+		__log("Cannot read file data/output.json")
+		return
+
+	var r := JSON.parse(b.get_string_from_utf8())
+	if r.error != OK:
+		__log("Error processing JSON")
+		__log(r.error_string)
+		return
+
+	__log("data/output.json : %s" % r.result)
+
+func __run_custom():
+	wasi_ctx.write_memory_file("test.py", code_textbox.text)
+	__run("test.py")
