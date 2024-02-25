78c937a82c3237498dd0345ebe5e231bf9a60dcb
Dheatly23 <71598333+Dheatly23@users.noreply.github.com>
2024-02-25 21:20:06 +0700

    Adds Camera Movement

---
+++ example/script/3DCamera.gd
@@ -0,0 +1,30 @@
+extends Camera
+
+const PI_2 := PI / 2
+const SPEED := PI / 4
+
+export var dist := 10.0
+
+var rx := PI / 4
+var ry := PI / -4
+
+func _process(delta: float):
+	delta *= SPEED
+
+	var up_pressed := Input.get_action_strength("camera_up") >= 0.1
+	var down_pressed := Input.get_action_strength("camera_down") >= 0.1
+	var left_pressed := Input.get_action_strength("camera_left") >= 0.1
+	var right_pressed := Input.get_action_strength("camera_right") >= 0.1
+
+	if up_pressed and not down_pressed:
+		ry = clamp(ry - delta, -PI_2, PI_2)
+	elif not up_pressed and down_pressed:
+		ry = clamp(ry + delta, -PI_2, PI_2)
+	if left_pressed and not right_pressed:
+		rx = wrapf(rx - delta, -PI, PI)
+	elif not left_pressed and right_pressed:
+		rx = wrapf(rx + delta, -PI, PI)
+
+	var q := Quat(Vector3(ry, rx, 0))
+	var v := q * (Vector3.BACK * dist)
+	transform = Transform(Basis(q), v)
