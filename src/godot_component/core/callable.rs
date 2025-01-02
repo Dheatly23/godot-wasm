1f1f53efed8fd3ea645bfcf31d72000b2da14630
Dheatly23 <71598333+Dheatly23@users.noreply.github.com>
2025-01-02 09:28:23 +0700

    Adds Many More Callable Methods

--- src/godot_component/core/callable.rs
+++ src/godot_component/core/callable.rs
@@ -12,9 +12,16 @@ filter_macro! {method [
     object -> "object",
     method_name -> "method-name",
     call -> "call",
+    call_deferred -> "call-deferred",
     callv -> "callv",
     bind -> "bind",
     bindv -> "bindv",
+    unbind -> "unbind",
+    get_argument_count -> "get-argument-count",
+    get_bound_arguments -> "get-bound-arguments",
+    get_bound_arguments_count -> "get-bound-arguments-count",
+    rpc -> "rpc",
+    rpc_id -> "rpc-id",
 ]}
 
 impl crate::godot_component::bindgen::godot::core::callable::Host
@@ -76,6 +83,21 @@ impl crate::godot_component::bindgen::godot::core::callable::Host
         self.set_var(r)
     }
 
+    fn call_deferred(
+        &mut self,
+        var: WasmResource<Variant>,
+        args: Vec<Option<WasmResource<Variant>>>,
+    ) -> AnyResult<()> {
+        filter_macro!(filter self.filter.as_ref(), godot_core, callable, call_deferred)?;
+        let v: Callable = self.get_value(var)?;
+        let a = args
+            .into_iter()
+            .map(|v| self.maybe_get_var(v))
+            .collect::<AnyResult<Vec<_>>>()?;
+        self.release_store(move || v.call_deferred(&a));
+        Ok(())
+    }
+
     fn callv(
         &mut self,
         var: WasmResource<Variant>,
@@ -111,5 +133,63 @@ impl crate::godot_component::bindgen::godot::core::callable::Host
         let v: Callable = self.get_value(var)?;
         let args: VariantArray = self.get_value(args)?;
         self.set_into_var(v.bindv(&args))
+    }
+
+    fn unbind(&mut self, var: WasmResource<Variant>, n: i64) -> AnyResult<WasmResource<Variant>> {
+        filter_macro!(filter self.filter.as_ref(), godot_core, callable, unbind)?;
+        let v: Callable = self.get_value(var)?;
+        self.set_into_var(v.unbind(n.try_into()?))
+    }
+
+    fn get_argument_count(&mut self, var: WasmResource<Variant>) -> AnyResult<i64> {
+        filter_macro!(filter self.filter.as_ref(), godot_core, callable, get_argument_count)?;
+        let v: Callable = self.get_value(var)?;
+        Ok(v.get_argument_count() as _)
+    }
+
+    fn get_bound_arguments(
+        &mut self,
+        var: WasmResource<Variant>,
+    ) -> AnyResult<WasmResource<Variant>> {
+        filter_macro!(filter self.filter.as_ref(), godot_core, callable, get_bound_arguments)?;
+        let v: Callable = self.get_value(var)?;
+        self.set_into_var(v.get_bound_arguments())
+    }
+
+    fn get_bound_arguments_count(&mut self, var: WasmResource<Variant>) -> AnyResult<i64> {
+        filter_macro!(filter self.filter.as_ref(), godot_core, callable, get_bound_arguments_count)?;
+        let v: Callable = self.get_value(var)?;
+        Ok(v.get_bound_arguments_count() as _)
+    }
+
+    fn rpc(
+        &mut self,
+        var: WasmResource<Variant>,
+        args: Vec<Option<WasmResource<Variant>>>,
+    ) -> AnyResult<()> {
+        filter_macro!(filter self.filter.as_ref(), godot_core, callable, rpc)?;
+        let v: Callable = self.get_value(var)?;
+        let a = args
+            .into_iter()
+            .map(|v| self.maybe_get_var(v))
+            .collect::<AnyResult<Vec<_>>>()?;
+        self.release_store(move || v.rpc(&a));
+        Ok(())
+    }
+
+    fn rpc_id(
+        &mut self,
+        var: WasmResource<Variant>,
+        peer_id: i64,
+        args: Vec<Option<WasmResource<Variant>>>,
+    ) -> AnyResult<()> {
+        filter_macro!(filter self.filter.as_ref(), godot_core, callable, rpc_id)?;
+        let v: Callable = self.get_value(var)?;
+        let a = args
+            .into_iter()
+            .map(|v| self.maybe_get_var(v))
+            .collect::<AnyResult<Vec<_>>>()?;
+        self.release_store(move || v.rpc_id(peer_id, &a));
+        Ok(())
     }
 }
